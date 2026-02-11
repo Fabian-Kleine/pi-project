158d5262496267fbae0c459e88cdf77817807ad9
Fabian.Kleine <fabiankleine800@gmail.com>
2026-02-11 11:52:31 +0100

    added new Routes for period

--- backend/src/Cleanup.php
+++ backend/src/Cleanup.php
@@ -2,15 +2,16 @@
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/../config.php';
 
-use Database;
+use Src\Database;
 
-echo("startet cleanup...");
+$dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
 
 try {
     $db = new Database($dsn, DB_USER, DB_PASS);
     
-    // $db->execute("DELETE FROM voc_sensor_data WHERE timestamp < datetime('now', '-30 days')");
-    $db->execute("DELETE FROM voc_sensor_data WHERE id=2");
+    $db->execute("DELETE FROM voc_sensor_data WHERE created_at < NOW() - INTERVAL 30 DAY");
+    $db->execute("DELETE FROM radar_sensor_data WHERE created_at < NOW() - INTERVAL 30 DAY");
+    echo("cleaned up...");
     
 } catch (Exception $e) {
     echo($e);
